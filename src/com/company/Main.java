ff21819ac3a0754ddf67d88c540a908d6225f8e1
lagmans <lagmans1@yandex.ru>
2021-10-10 22:04:09 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String line = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
